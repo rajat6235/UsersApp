379e3b800bc3bc459caa7b997560f38e0c4c53fc
rajat6235 <guptarajat234@gmail.com>
2022-10-12 12:46:02 +0530

    users code cleanup

--- usersapp/src/components/Users/Users.js
+++ usersapp/src/components/Users/Users.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react'
-import GetData from '../../utilities/services'
-import Pagination from '../Pagination/Pagination';
-import './Users.css'
+import React, { useEffect, useRef, useState } from "react";
+import GetData from "../../utilities/services";
+import Pagination from "../Pagination/Pagination";
+import "./Users.css";
 
 function Users() {
   const [data, setData] = useState([]);
@@ -14,38 +14,41 @@ function Users() {
     GetData().then((response) => setData(response.data));
   }, []);
 
-
-  useEffect(()=>{
+  useEffect(() => {
     const setSpans = () => {
       console.log(imageRef.current.clientHeight);
-    }
-    const imgref =imageRef.current
-   imgref? imgref.addEventListener("click", setSpans): console.log('no data');
-  }
-  )
- const lastPostIndex = currentPage * postsPerPage;
-    const firstPostIndex = lastPostIndex - postsPerPage;
-    const currentPosts = data.slice(firstPostIndex, lastPostIndex);
-    const images =     data && currentPosts.map(({id,author,width,height,download_url},index)=>(
+    };
+    const imgref = imageRef.current;
+    imgref
+      ? imgref.addEventListener("click", setSpans)
+      : console.log("no data");
+  });
+  const lastPostIndex = currentPage * postsPerPage;
+  const firstPostIndex = lastPostIndex - postsPerPage;
+  const currentPosts = data.slice(firstPostIndex, lastPostIndex);
+  const images =
+    data &&
+    currentPosts.map(({ id, author, width, height, download_url }, index) => (
       <div className="pics">
-       <img src={download_url} style={{width:'100%'}}ref={imageRef} alt='user'></img>
+        <img
+          src={download_url}
+          style={{ width: "100%" }}
+          ref={imageRef}
+          alt="user"
+        ></img>
       </div>
-          ))
+    ));
   return (
     <div>
-    <div className="image-list">
-      
-  {images}
-      
+      <div className="image-list">{images}</div>
+      <Pagination
+        totalPosts={data.length}
+        postsPerPage={postsPerPage}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+      />
     </div>
-    <Pagination
-                totalPosts={data.length}
-                postsPerPage={postsPerPage}
-                setCurrentPage={setCurrentPage}
-                currentPage={currentPage}
-            />
-    </div>
-  )
+  );
 }
 
-export default Users
+export default Users;
