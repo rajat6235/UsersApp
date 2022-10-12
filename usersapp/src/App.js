b448b7ebca7454fae45d04792a40993bb15a3f24
rajat6235 <guptarajat234@gmail.com>
2022-10-12 07:36:55 +0530

    folder structure

--- usersapp/src/App.js
+++ usersapp/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Users from './components/Users'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Users/>
 <p> esf</p>
     </div>
   );
---
+++ usersapp/src/components/Users/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div>indjbhbhex</div>
+  )
+}
+
+export default index
