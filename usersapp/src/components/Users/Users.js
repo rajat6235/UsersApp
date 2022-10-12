f9bd64e70c36afc65ed8cf38547342b00ee06b28
rajat6235 <guptarajat234@gmail.com>
2022-10-12 12:45:03 +0530

    images styling and responsiveness

--- usersapp/src/components/Users/Users.js
+++ usersapp/src/components/Users/Users.js
@@ -1,38 +1,49 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import GetData from '../../utilities/services'
 import Pagination from '../Pagination/Pagination';
+import './Users.css'
 
 function Users() {
   const [data, setData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage, setPostsPerPage] = useState(20);
 
+  const imageRef = useRef();
+
   useEffect(() => {
     GetData().then((response) => setData(response.data));
   }, []);
 
+
+  useEffect(()=>{
+    const setSpans = () => {
+      console.log(imageRef.current.clientHeight);
+    }
+    const imgref =imageRef.current
+   imgref? imgref.addEventListener("click", setSpans): console.log('no data');
+  }
+  )
  const lastPostIndex = currentPage * postsPerPage;
     const firstPostIndex = lastPostIndex - postsPerPage;
     const currentPosts = data.slice(firstPostIndex, lastPostIndex);
+    const images =     data && currentPosts.map(({id,author,width,height,download_url},index)=>(
+      <div className="pics">
+       <img src={download_url} style={{width:'100%'}}ref={imageRef} alt='user'></img>
+      </div>
+          ))
   return (
     <div>
+    <div className="image-list">
       
-      {
-         data && currentPosts.map(({id,author,width,height,download_url},index)=>(
-     <div>
-      {console.log(download_url)}
-      <img src={download_url} style={{height:'100px'}} alt='user'></img>
-     </div>
-         ))
-      }
+  {images}
       
-      <Pagination
+    </div>
+    <Pagination
                 totalPosts={data.length}
                 postsPerPage={postsPerPage}
                 setCurrentPage={setCurrentPage}
                 currentPage={currentPage}
             />
-
     </div>
   )
 }
